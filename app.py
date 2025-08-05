205f8fe907b046427dc6c7b0c260dc7fdd5702bd
Paolo Brandoli <paolo@binarno.com>
2025-08-05 11:07:51 +0200

    Digitally sign configuration

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import urllib.parse
+import Crypto.Signature
+import Crypto.PublicKey.RSA
+import Crypto.Signature.pkcs1_15
+import Crypto.Hash.SHA256
+import base64
+import os
 
 if 'configuring' not in st.session_state:
     st.session_state.configuring = False
@@ -48,7 +54,24 @@ else:
     """)
 
     parameters = {'authUser': st.session_state.auth_user, 'authPassword': st.session_state.auth_password, 'account': st.session_state.sip_address}
-    url = 'tiephoneconfig://open?' + urllib.parse.urlencode(parameters)
+    encoded_parameters = urllib.parse.urlencode(parameters)
+
+    url = 'tiephoneconfig://open/?' + encoded_parameters
+
+    encoded_url_for_signature = url.encode('UTF-8')
+
+    hash = Crypto.Hash.SHA256.SHA256Hash(encoded_url_for_signature)
+    hash16 = hash.hexdigest()
+
+    with open("configuration.key") as signature_file:
+        signarure_key_content = signature_file.read()
+    password = os.getenv("SIGNATURE_PASSWORD")
+    key = Crypto.PublicKey.RSA.import_key(signarure_key_content, password)
+    signature_object = Crypto.Signature.pkcs1_15.new(key)
+    signature = signature_object.sign(hash)
+    signature_base64 = base64.b64encode(signature).decode('UTF-8')
+
+    url = url + "&signature=" + signature_base64
     st.link_button('Almost there! Click here to configure Jeanie', url)
     st.markdown(f"""
     Configuration URL: [{url}]({url})
